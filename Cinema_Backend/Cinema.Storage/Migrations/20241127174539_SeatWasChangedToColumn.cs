0f093b156040e38c652bc1d3369795ef9fcbf4bc
Nikita Developer <nik0609tak@gmail.com>
2024-11-27 18:48:45 +0100

    Miugrations

---
+++ Cinema_Backend/Cinema.Storage/Migrations/20241127174539_SeatWasChangedToColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinema.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeatWasChangedToColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "seat",
+                table: "seats",
+                newName: "column");
+
+            migrationBuilder.RenameIndex(
+                name: "unique_row_seat",
+                table: "seats",
+                newName: "unique_row_column");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "column",
+                table: "seats",
+                newName: "seat");
+
+            migrationBuilder.RenameIndex(
+                name: "unique_row_column",
+                table: "seats",
+                newName: "unique_row_seat");
+        }
+    }
+}
